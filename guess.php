09f0327035a6dd1abf30cc72330c117c674fff3e
Sarvjeet Singh <sarvjeet.19b121001@abes.ac.in>
2020-06-23 10:21:49 +0530

    Initial commit

---
+++ guess.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Sarvjeet Singh ad0e1110</title>
+</head>
+<body>
+<h1>Welcome to guessing game</h1>
+<p>
+<?php
+  if ( ! isset($_GET['guess']) ) { 
+    echo("Missing guess parameter");
+  } else if ( strlen($_GET['guess']) < 1 ) {
+    echo("Your guess is too short");
+  } else if ( ! is_numeric($_GET['guess']) ) {
+    echo("Your guess is not a number");
+  } else if ( $_GET['guess'] < 65 ) {
+    echo("Your guess is too low");
+  } else if ( $_GET['guess'] > 65 ) {
+    echo("Your guess is too high");
+  } else {
+    echo("Congratulations - You are right");
+  }
+?>
+</p>
+</body>
+</html>
+  
